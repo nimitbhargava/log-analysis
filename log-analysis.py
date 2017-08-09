fde248ef9b9c5eb30cabc6781d9c7f2920673a44
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-09 13:58:56 +0530

    Added shebang and comment on python project

---
+++ log-analysis.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# 
+# A reporting tool that prints out reports (in plain text) based on the data 
+# in the database. This reporting tool is a Python program using the psycopg2
+# module to connect to the database.
