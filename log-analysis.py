025d88f7c18f0c46c15a207d6b565a873defc772
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-09 14:30:27 +0530

    Added main function

--- log-analysis.py
+++ log-analysis.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-# 
-# A reporting tool that prints out reports (in plain text) based on the data 
+#
+# A reporting tool that prints out reports (in plain text) based on the data
 # in the database. This reporting tool is a Python program using the psycopg2
 # module to connect to the database.
+
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
+    
