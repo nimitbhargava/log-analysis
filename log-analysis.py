23e55d043bd85238488bf3911b85fbf9f97e3861
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-09 15:13:12 +0530

    Queries to execute and print desired formatted results

--- log-analysis.py
+++ log-analysis.py
@@ -12,7 +12,40 @@ def main():
     DBNAME = 'news'
 
     db = psycopg2.connect(database = DBNAME)
-  
+
+    queryOne = """select title, count(*) as views from log inner join articles
+     on concat('/article/', articles.slug) = log.path where status='200 OK' 
+     and path like '%article%' group by title order by views desc limit 3;"""
+    
+    queryTwo = """select name, count(*) as views from authors join articles 
+     on authors.id = articles.author join log on log.path = 
+     concat('/article/', articles.slug) group by name order by views desc;"""
+    
+    queryThree = """select total_views.date, 
+    (100.0*error_views.errors/total_views.views) as percent from total_views 
+    join error_views on total_views.date = error_views.date and 
+    (100.0*error_views.errors/total_views.views) > 1;"""
+    
+    print("\n\n1. What are the most popular three articles of all time?")
+    for(title, views) in fetchAll(db, queryOne):
+        print("\"{}\" -- {} views".format(title, views))
+        
+    print("\n\n2. Who are the most popular article authors of all time?")
+    for(title, views) in fetchAll(db, queryTwo):
+        print("{} -- {} views".format(title, views))
+        
+    print("\n\n3. On which days did more than 1% of requests lead to errors?")
+    for(date, errors) in fetchAll(db, queryThree):
+        print("{} -- {}% errors".format(date, errors))
+        
+    db.close()
+    
+
+def fetchAll(db, query):
+    c = db.cursor()
+    c.execute(query)
+    return c.fetchall()
+    
     
 if __name__ == '__main__':
     main()
