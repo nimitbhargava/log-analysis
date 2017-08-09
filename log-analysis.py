5de379b8490c320b5c3d4afe0986a8a963fab365
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-09 16:22:11 +0530

    Removed repeated code

--- log-analysis.py
+++ log-analysis.py
@@ -13,26 +13,27 @@ def main():
 
     db = psycopg2.connect(database = DBNAME)
 
-    queryOne = """select title, count(*) as views from log inner join articles
+    queryOne = """select concat('"',title,'"'), concat(count(*), ' views') as views from log inner join articles
      on concat('/article/', articles.slug) = log.path where status='200 OK' 
      and path like '%article%' group by title order by views desc limit 3;"""
-    queryTwo = """select name, count(*) as views from authors join articles 
+    queryTwo = """select name, concat(count(*), ' views') as views from authors join articles 
      on authors.id = articles.author join log on log.path = 
      concat('/article/', articles.slug) group by name order by views desc;"""
     queryThree = """select to_char(total_views.date, 'Month dd, YYYY'), 
-    trunc(100.0*error_views.errors/total_views.views, 1) as percent from
+    concat(trunc(100.0*error_views.errors/total_views.views, 1), '% errors') as percent from
     total_views join error_views on total_views.date = error_views.date and 
     (100.0*error_views.errors/total_views.views) > 1;"""
     print("\n\n1. What are the most popular three articles of all time?")
-    for(title, views) in fetchAll(db, queryOne):
-        print("\"{}\" -- {} views".format(title, views))
+    printQueryResult(db, queryOne)
     print("\n\n2. Who are the most popular article authors of all time?")
-    for(title, views) in fetchAll(db, queryTwo):
-        print("{} -- {} views".format(title, views))
+    printQueryResult(db, queryTwo)
     print("\n\n3. On which days did more than 1% of requests lead to errors?")
-    for(date, errors) in fetchAll(db, queryThree):
-        print("{} -- {}% errors".format(date, errors))        
+    printQueryResult(db, queryThree)
     db.close()    
+
+def printQueryResult(db, queryOne):
+    for(title, value) in fetchAll(db, queryOne):
+        print("{} -- {}".format(title, value))
 
 
 def fetchAll(db, query):
